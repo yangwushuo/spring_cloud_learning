3daf46f2cb098c6b7f23f8aab4a08308a130eac2
yangwushuo <1197414647@qq.com>
2022-10-10 01:14:40 +0800

    22-10-10

---
+++ spring_cloud_01_eureka_server/src/main/java/com/jason/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.jason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yangwushuo
+ * @time 2022/10/9 15:10
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
---
+++ spring_cloud_02_eureka_client/src/main/java/com/jason/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.jason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yangwushuo
+ * @time 2022/10/9 19:41
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class,args);
+    }
+}
